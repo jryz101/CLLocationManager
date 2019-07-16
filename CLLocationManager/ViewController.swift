ab75a638259540baa39590d34d5933074a766834
JerryTan <jryz101@gmail.com>
2019-07-16 21:31:03 +0800

    2. Imported Mapkit and CoreLocation frameworks, and add privacy user location when in use description in info.plist.

--- CLLocationManager/ViewController.swift
+++ CLLocationManager/ViewController.swift
@@ -1,12 +1,12 @@
 //  ViewController.swift
 //  CLLocationManager
-//
 //  Created by Jerry Tan on 16/07/2019.
 //  Copyright © 2019 Starknet Technologies®. All rights reserved.
 
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
