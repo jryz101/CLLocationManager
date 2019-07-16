f7e03cde1bc1d18d49a590b1b59ad631cdc35d44
JerryTan <jryz101@gmail.com>
2019-07-16 21:25:26 +0800

    1. Add MKMapView object to view controller, setup auto layouts mapView constraints.

--- CLLocationManager/ViewController.swift
+++ CLLocationManager/ViewController.swift
@@ -1,20 +1,20 @@
-//
 //  ViewController.swift
 //  CLLocationManager
 //
 //  Created by Jerry Tan on 16/07/2019.
 //  Copyright © 2019 Starknet Technologies®. All rights reserved.
-//
+
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
+    @IBOutlet weak var map: MKMapView!
 }
 
