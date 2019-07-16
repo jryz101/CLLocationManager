dc441c86ce754b64691b7327c9e55809c47bcaaf
JerryTan <jryz101@gmail.com>
2019-07-16 22:28:25 +0800

    3. Initialise CLLocationManager in viewDidLoad, compose requestWhenInUseAuthorization, specified CLLocationDegrees, compose MKCoordinateSpan and set region.

--- CLLocationManager/ViewController.swift
+++ CLLocationManager/ViewController.swift
@@ -9,9 +9,39 @@ import MapKit
 import CoreLocation
 
 class ViewController: UIViewController {
+    
+    //Declare a variable object that use to start and stop the delivery of location-related events to the app
+    var locationManager: CLLocationManager?
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Set locationManager equals to CLLocationManager( )
+        locationManager = CLLocationManager()
+        
+        //Requests permission to use location services while the app is in the foreground
+        locationManager?.requestWhenInUseAuthorization()
+        
+        //The geographical coordinate information
+        if let coordinate = locationManager?.location?.coordinate {
+            
+            //A latitude or longitude value specified in degrees
+            let xScale:CLLocationDegrees = 0.01
+            let yScale:CLLocationDegrees = 0.01
+            
+            //The width and height of a map region.
+            let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: yScale, longitudeDelta: xScale)
+            
+            //A rectangular geographic region centered around a specific latitude and longitude
+            let region = MKCoordinateRegion(center: coordinate, span: span)
+            
+            //Changes the currently visible region and optionally animates the change.
+            map.setRegion(region, animated: true)
+            
+        }
+        
     }
     
     
